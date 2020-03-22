106e6e1075625502b46074ba1e917518655192a3
Peter <petertran198@gmail.com>
2020-03-21 20:07:46 -0400

    Added dynamic show page for user and created edit funtionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
     def show 
+        @user = User.find(params[:id])
+    end
 
+    def edit 
+        @user = User.find(current_user.id)
+
+    end 
+    def update 
+        
+        if current_user.update(user_params)
+            redirect_to current_user
+        else
+
+        end
+    end
+
+    private 
+    def user_params 
+        params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
     end
 end
