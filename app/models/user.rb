15954154be3824d12ad46b203d5c545719ebe232
Peter <petertran198@gmail.com>
2020-03-21 20:37:30 -0400

    Added post model and create 1 to many association between post and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy #indicates that all the associated posts will be destroyed when user destroyed.
+
 end
