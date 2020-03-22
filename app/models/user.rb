3ac34b23cc323dc905b8c3fee1a07aa5e8c7b8a5
Peter <petertran198@gmail.com>
2020-03-22 18:00:43 -0400

    Made it that pictures on index page clickable, redirect to their users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy #indicates that all the associated posts will be destroyed when user destroyed.
   serialize :following, Array #this is to trick the following attribute which is of text attribute to behave like an array so I can push users in 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 15}
+  
+  
   has_one_attached :avatar
 end
